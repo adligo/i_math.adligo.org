303f54bc0e5de95db506b6101fb8866c42b5897a
Scott Morgan <scott@adligo.com>
2023-04-30 17:41:10 -0500

    Some inital work

---
+++ src/org/adligo/i_math/shared/I_EgcdResult.java
@@ -0,0 +1,50 @@
+package org.adligo.i_math.shared;
+
+import java.math.BigInteger;
+
+/**
+ * <br/>
+ * <br/>
+ * 
+ * @author scott<br/>
+ *         <br/>
+ * 
+ * <pre><code>
+ * ---------------- Apache ICENSE-2.0 --------------------------
+ *
+ * Copyright 2022 Adligo Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </code><pre>
+ */
+public interface I_EgcdResult {
+
+  /**
+   * Get the Bézout coefficient corresponding to the coefficient A of the Extended GCD A function;
+   * {@link https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm}
+   * @return
+   */
+  BigInteger getBca();
+  
+  /**
+  * Get the Bézout coefficient corresponding to the coefficient A of the Extended GCD A function;
+  * {@link https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm}
+  */
+  BigInteger getBcb();
+  
+  /**
+   * Get the result of the Greatest Common Divisor 
+   * @return
+   */
+  BigInteger getResult();
+}
---
+++ src/org/adligo/i_math/shared/I_MathCtx.java
@@ -0,0 +1,41 @@
+package org.adligo.i_math.shared;
+
+import java.math.BigInteger;
+
+/**
+ * This is a interface to a set of Math Functions so that stubbing can be facilitated easily with out calling the functions<br/>
+ * <br/>
+ * 
+ * @author scott<br/>
+ *         <br/>
+ * 
+ * <pre><code>
+ * ---------------- Apache ICENSE-2.0 --------------------------
+ *
+ * Copyright 2022 Adligo Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </code><pre>
+ */
+public interface I_MathCtx {
+  
+  /**
+   * https://en.wikipedia.org/wiki/Euclidean_algorithm
+   * @param the a
+   * @param b
+   * @return
+   */
+  BigInteger gcd(BigInteger a,BigInteger b);
+	
+  I_EgcdResult egcd(BigInteger a, BigInteger b);
+}
